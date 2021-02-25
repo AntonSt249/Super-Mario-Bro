4e6ba4cc29154ef7c81585c6f4a081bccbde1a30
sterzinge_a1 <anton.sterzinger@preh.de>
2021-02-25 15:33:04 +0100

    Created project and added first elements (jumping doesnt work)

---
+++ Super_Mario _Bro/Super_Mario _Bro/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Super_Mario__Bro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ground
+            // 
+            this.ground.BackColor = System.Drawing.Color.Brown;
+            this.ground.Location = new System.Drawing.Point(0, 579);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(484, 33);
+            this.ground.TabIndex = 0;
+            this.ground.TabStop = false;
+            this.ground.Tag = "platform";
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Blue;
+            this.player.Location = new System.Drawing.Point(47, 523);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(30, 50);
+            this.player.TabIndex = 1;
+            this.player.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 611);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.ground);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Super_Mario _Bro/Super_Mario _Bro/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Super_Mario__Bro
+{
+    public partial class Form1 : Form
+    {
+        bool goLeft = false;
+        bool goRight = false;
+        bool jumping = false;
+
+        int jumpSpeed = 10;
+        int force = 8;
+        int score = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+            if (e.KeyCode == Keys.Space && !jumping)
+            {
+                jumping = true;
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+            if (jumping)
+            {
+                jumping = false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            player.Top += jumpSpeed;
+
+            if (jumping && force < 0)
+            {
+                jumping = false;
+            }
+            if (goLeft)
+            {
+                player.Left -= 5;
+            }
+            if (goRight)
+            {
+                player.Left += 5;
+            }
+            if (jumping)
+            {
+                jumpSpeed = -12;
+                force = -1;
+            }
+            else
+            {
+                jumpSpeed = 12;
+            }
+
+            foreach (Control control in this.Controls)
+            {
+                if (control is PictureBox && control.Tag == "platform")
+                {
+                    if (player.Bounds.IntersectsWith(control.Bounds) && !jumping)
+                    {
+                        force = 8;
+                        player.Top = control.Top - player.Height;
+                    }
+                }
+            }
+        }
+    }
+}
