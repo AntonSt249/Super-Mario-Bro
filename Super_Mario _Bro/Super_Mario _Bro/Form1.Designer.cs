75f4cb96b9a4323306f5c75b93fa2b25ca5fc10b
Anton Müller <anton.st249@gmx.deuser.name>
2021-03-02 19:32:46 +0100

    downloaded new textures, added them for making a sidescroller

--- Super_Mario _Bro/Super_Mario _Bro/Form1.Designer.cs
+++ Super_Mario _Bro/Super_Mario _Bro/Form1.Designer.cs
@@ -32,16 +32,21 @@
             this.ground = new System.Windows.Forms.PictureBox();
             this.player = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.background = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.background)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // ground
             // 
             this.ground.BackColor = System.Drawing.Color.Brown;
-            this.ground.Location = new System.Drawing.Point(0, 579);
+            this.ground.Location = new System.Drawing.Point(0, 713);
+            this.ground.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ground.Name = "ground";
-            this.ground.Size = new System.Drawing.Size(484, 33);
+            this.ground.Size = new System.Drawing.Size(645, 41);
             this.ground.TabIndex = 0;
             this.ground.TabStop = false;
             this.ground.Tag = "platform";
@@ -49,9 +54,10 @@
             // player
             // 
             this.player.BackColor = System.Drawing.Color.Blue;
-            this.player.Location = new System.Drawing.Point(47, 523);
+            this.player.Location = new System.Drawing.Point(63, 644);
+            this.player.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.player.Name = "player";
-            this.player.Size = new System.Drawing.Size(30, 50);
+            this.player.Size = new System.Drawing.Size(40, 62);
             this.player.TabIndex = 1;
             this.player.TabStop = false;
             // 
@@ -61,20 +67,46 @@
             this.timer1.Interval = 20;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // background
+            // 
+            this.background.Image = global::Super_Mario__Bro.Properties.Resources.background1;
+            this.background.Location = new System.Drawing.Point(0, 0);
+            this.background.Name = "background";
+            this.background.Size = new System.Drawing.Size(2000, 480);
+            this.background.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.background.TabIndex = 2;
+            this.background.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Super_Mario__Bro.Properties.Resources.platform;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 428);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(552, 43);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "platform";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 611);
+            this.ClientSize = new System.Drawing.Size(1924, 473);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.background);
             this.Controls.Add(this.player);
             this.Controls.Add(this.ground);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Super Mario Bro";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
             ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.background)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -83,6 +115,8 @@
         private System.Windows.Forms.PictureBox ground;
         private System.Windows.Forms.PictureBox player;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox background;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
